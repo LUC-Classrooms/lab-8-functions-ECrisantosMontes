8bf1656416998dd08b2c1cf36131f8078dd7c985
ECrisantosMontes <156939016+ECrisantosMontes@users.noreply.github.com>
2024-04-03 23:50:38 +0000

    Lab 8- eyeballs with top-hat staring into your soul done! (joke btw)

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function setup() {
   createCanvas(600, 400);
+  this.color = color(random(255), random(255), random(255));// random eye color
 }
 
 function draw() {
-  background(200);
+  background(255, 50, 140);
 
   //this is a call to myShape()
   myShape(width / 2, height / 2, 1);
+  myShape(-100, 40, 1);
   // myShape() takes three arguments
   // arg1: horizontal location
   // arg2: vertical location
@@ -14,6 +16,7 @@ function draw() {
   /* add two more calls to myShape that draw your shape in different locations
   */
   
+  myShape(width / 4, height / 4, 1);
 }
 
 function myShape(x, y, s) {
@@ -21,8 +24,18 @@ function myShape(x, y, s) {
   push(); // make a separate layer
   translate(x, y); // move the origin point
   scale(s);
-  
-  ellipse(0, 0, w, h); // simple ellipse at the translated origin (0,0)
-  
-  pop(); // dispose of the layer
+  strokeWeight(1)
+  fill(250)
+  ellipse(0, 0, 60); // eyeball
+  fill(this.color)//random eye color
+  ellipse(0, 0, 40) //iris
+  fill(0);// black pupil
+  ellipse(0, 0, 20)//pupil
+  //...eyeballs with a top-hat because why not?
+  strokeCap(SQUARE);
+  strokeWeight(8);//thick line
+  line(30, -30, -30, -30); //easier than making a skinny rectangle
+  fill(0);
+  rect(-19,-85, width / 16, height / 8); // top part of top-hat
+  //pop(); // dispose of the layer
 }
